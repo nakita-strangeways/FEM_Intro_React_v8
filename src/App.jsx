82078fd39ff8a118a982a843c9b8f5e040207fb8
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2023-09-15 14:27:07 -0700

    completed core react concepts - jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Doink",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pogo",
-      breed: "African Grey",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet animal="Dog" breed="Havanese" name="Luna" />
+      <Pet animal="Cat" breed="Mix" name="Doink" />
+      <Pet animal="Bird" breed="African Grey" name="Pogo" />
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
