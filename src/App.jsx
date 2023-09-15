23638387a5ebf3b3ac3b99f539a2580579e3bac1
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2023-09-15 14:40:08 -0700

    adding location hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet animal="Dog" breed="Havanese" name="Luna" />
-      <Pet animal="Cat" breed="Mix" name="Doink" />
-      <Pet animal="Bird" breed="African Grey" name="Pogo" />
+      <SearchParams />
     </div>
   )
 };
